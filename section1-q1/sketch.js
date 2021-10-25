00e58e2243533202b9215c3ef77adf984f3111dc
Tanama1006 <tanama1006anything@gmail.com>
2021-10-26 01:24:54 +0900

    section2-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,15 +1,14 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  arc(25 + 50, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
-
+  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI)
+  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
@@ -17,4 +16,14 @@ function setup(){
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  fill(blue);
+  noStroke();
+  ellipse(300, 100, 100, 100)
+  fill(255);
+  ellipse(260, 80,90,80);
+  fill(green);
+  ellipse(290,90,90,90);
+  fill(255);
+  ellipse(300,85,90,90);
 }

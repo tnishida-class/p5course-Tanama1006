02f30a8d094c41cadd7584b97de8581ac64d81ea
Tanama1006 <tanama1006anything@gmail.com>
2021-10-26 02:44:01 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -4,7 +4,16 @@ function setup() {
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
+  for(let j = 0; j < 8; j++){
+      console.log(i,j);
+      rect(20*i,20*j,20,20);
+      if(i*j %2==0){
+        noFill()
+      }
+    else{
+        fill(51)
+    }
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }

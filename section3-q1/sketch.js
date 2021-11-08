2694c4d8fd51dde1bdd59c0fd8a62b6ef75680aa
Tanama1006 <tanama1006anything@gmail.com>
2021-11-09 02:39:14 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,20 @@
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
+// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」キーを押す→早い
 let count;
 let cycle;
+count=0;
+cycle=100;
 
 function setup(){
-  createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
+  createCanvas(200,200);
 }
 
 function draw(){
-  background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  background(160,192,255);
+  if(keyIsDown("".charCodeAt(0))){count=(Count+2)%cycle;}
+  else{count=(count+1)%cycle;}
+  if(count>60){size=count}
+  else{size=100-count}
+  ellipse(width/2,height/2,size);
+
+
 }
